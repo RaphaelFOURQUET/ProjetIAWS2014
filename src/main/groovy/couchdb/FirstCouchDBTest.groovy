9a8d82baf17476dd406e329c9bd4e121ee4720ed
RaphaelFOURQUET <raphikifourque@gmail.com>
2014-03-31 18:21:09 +0200

    #3 dependances

---
+++ src/main/groovy/couchdb/FirstCouchDBTest.groovy
@@ -0,0 +1,12 @@
+package couchdb
+
+//L'import génere une erreur maven test .....
+//import com.fourspaces.couchdb.Session
+
+
+class FirstCouchDBTest {
+	
+	//l'import jar couchdb4j ne semble pas marcher
+	//Session s = new Session("localhost", 5984)
+
+}
