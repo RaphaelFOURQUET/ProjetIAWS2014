132f067c8f79b836b973d217ef71cddc5627df42
RaphaelFOURQUET <raphikifourque@gmail.com>
2014-04-02 17:35:59 +0200

    remaniement keyLogs

---
+++ src/main/groovy/data/MyLogsData.groovy
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package data
+
+/**
+ * @author Raphaël
+ *
+ */
+class MyLogsData {
+	
+	private static final String TISSEO_KEY = "a03561f2fd10641d96fb8188d209414d8"
+	
+	private static final String VELO_KEY = "9882ff33d68063c700fd4764aca42067f4cb6b21"
+	
+	private MyLogsData() {}
+	
+
+}
